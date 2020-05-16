f4fa5fc0bc2a15d3b43c30d39ad7d63fecafbc64
Asura <53406857+Rohith-Pillalamarri@users.noreply.github.com>
2020-05-17 00:23:37 +0530

    Update Android.mk

--- msm8937-common/Android.mk
+++ msm8937-common/Android.mk
@@ -124,6 +124,27 @@ LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := TimeWeather
+LOCAL_MODULE_OWNER := motorola
+LOCAL_SRC_FILES := proprietary/app/TimeWeather/TimeWeather.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := com.motorola.timeweatherwidget.library
+LOCAL_MODULE_OWNER := motorola
+LOCAL_SRC_FILES := proprietary/system/framework/com.motorola.timeweatherwidget.library.jar
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := JAVA_LIBRARIES
+LOCAL_MODULE_SUFFIX := .jar
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := QtiTelephonyServicelibrary
 LOCAL_MODULE_OWNER := motorola
 LOCAL_SRC_FILES := proprietary/framework/QtiTelephonyServicelibrary.jar
